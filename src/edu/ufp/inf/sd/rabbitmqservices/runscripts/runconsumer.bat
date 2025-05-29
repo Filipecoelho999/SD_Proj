9f4eb8c63d685b451ded50574439df6dd305a6e0
JoaoRocha13 <42808@ufp.edu.pt>
2025-05-29 16:07:29 +0100

    minor fix, permissao de escrita

--- src/edu/ufp/inf/sd/rabbitmqservices/runscripts/runconsumer.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo === Iniciando Consumer RabbitMQ ===
-
-java -cp "C:\Users\Utilizador\Desktop\uni2\SDProj\out\production\SD;C:\Users\Utilizador\Desktop\uni2\SDProj\lib\*" edu.ufp.inf.sd.rabbitmqservices.drive.SubscribeClient
-
-pause
